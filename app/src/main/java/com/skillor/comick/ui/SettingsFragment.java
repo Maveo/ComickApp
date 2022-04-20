47dbdb1b2e93148d3588b4a7b77a4589f146aa56
skillor <skillor@gmx.net>
2022-04-20 15:36:36 +0200

    fix: settings attach to activity

--- app/src/main/java/com/skillor/comick/ui/SettingsFragment.java
+++ app/src/main/java/com/skillor/comick/ui/SettingsFragment.java
@@ -1,5 +1,6 @@
 package com.skillor.comick.ui;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -15,14 +16,18 @@ import com.skillor.comick.R;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
+    private SharedPreferences.OnSharedPreferenceChangeListener listener;
+
     private void refreshScreen() {
         setPreferenceScreen(null);
         addPreferencesFromResource(R.xml.root_preferences);
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        SharedPreferences.OnSharedPreferenceChangeListener listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+
+        listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
             public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
                 if (key.equals(getString(R.string.use_external_files_key))) {
                     ((MainActivity) requireActivity()).loadDirectory();
@@ -35,7 +40,16 @@ public class SettingsFragment extends PreferenceFragmentCompat {
         };
 
         PreferenceManager.getDefaultSharedPreferences(requireActivity()).registerOnSharedPreferenceChangeListener(listener);
+    }
 
+    @Override
+    public void onDetach() {
+        PreferenceManager.getDefaultSharedPreferences(requireActivity()).unregisterOnSharedPreferenceChangeListener(listener);
+        super.onDetach();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 
